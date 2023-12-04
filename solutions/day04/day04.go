202dbdc230304bf5539d4bbaf0a9c966588d4f35
tomaskul <>
2023-12-04 12:31:49 +0000

    Day 04 part 2 WIP solution

--- solutions/day04/day04.go
+++ solutions/day04/day04.go
@@ -19,6 +19,12 @@ type scratchcard struct {
 	actualValues  []int
 }
 
+type node struct {
+	parent   *scratchcard
+	data     scratchcard
+	children []node
+}
+
 func (c *scratchcard) calculatePoints() int {
 	result := 0
 
@@ -46,12 +52,15 @@ func (s *Day04) getData() {
 func (s *Day04) PrintPart1() {
 	s.getData()
 
-	scratchcards := parseInputData(s.rows)
-	actual := make([]int, 0)
-	for _, scratchcard := range scratchcards {
-		actual = append(actual, scratchcard.calculatePoints())
+	fmt.Println(util.Sum(evaluateScratchcards(parseInputData(s.rows))))
+}
+
+func evaluateScratchcards(input []scratchcard) []int {
+	result := make([]int, 0)
+	for _, scratchcard := range input {
+		result = append(result, scratchcard.calculatePoints())
 	}
-	fmt.Println(util.Sum(actual))
+	return result
 }
 
 func parseInputData(rows []string) []scratchcard {
@@ -87,3 +96,76 @@ func getIntValues(input []string) []int {
 func (s *Day04) PrintPart2() {
 	s.getData()
 }
+
+func traverseCards(allCards []scratchcard) {
+	cardLookup := createCardLookup(allCards)
+
+	root := node{parent: nil, data: cardLookup[1]}
+	root.children = root.getChildren(cardLookup)
+	for _, childNode := range root.children {
+
+	}
+}
+
+func createCardLookup(cards []scratchcard) map[int]scratchcard {
+	result := make(map[int]scratchcard, 0)
+	for _, card := range cards {
+		result[card.id] = card
+	}
+	return result
+}
+
+func (n *node) getChildren(cardLookup map[int]scratchcard) []node {
+	matchingNumbers := n.data.calculatePoints()
+	if matchingNumbers == 0 {
+		return []node{}
+	}
+	result := []node{}
+	for i := 1; i < matchingNumbers+1; i++ {
+		result = append(result, node{parent: &n.data, data: cardLookup[n.data.id+i]})
+	}
+	return result
+}
+
+// func findNewChildNodes(dest *[]node, data []scratchcard, cardLookup map[int]scratchcard) {
+
+// }
+
+// func pathThroughScratchcards(allCards []scratchcard) map[int]int {
+// 	result := make(map[int]int)
+// 	for _, scCard := range allCards {
+// 		result[scCard.id] = 0
+// 	}
+
+// 	cardsCurrentlyPossessed := []scratchcard{allCards[0]}
+
+// 	inPlay = true
+// 	for inPlay {
+// 		wins := evaluateScratchcardsPt2(cardsCurrentlyPossessed)
+// 		for id, nextSet := range wins {
+// 			result[id] = result[id] + nextSet
+
+// 			cardsCurrentlyPossessed = append(cardsCurrentlyPossessed, allCards)
+// 		}
+// 	}
+
+// 	return result
+// }
+
+// func evaluateScratchcardsPt2(input []scratchcard) map[int]int {
+// 	result := make(map[int]int, 0)
+// 	for _, scratchcard := range input {
+// 		nextSet := scratchcard.calculatePoints()
+// 		if nextSet == 0 {
+// 			continue
+// 		}
+
+// 		currentCount, ok := result[scratchcard.id]
+// 		if ok {
+// 			result[scratchcard.id] = currentCount + nextSet
+// 		} else {
+// 			result[scratchcard.id] = nextSet
+// 		}
+// 	}
+// 	return result
+// }
--- solutions/day04/day04_test.go
+++ solutions/day04/day04_test.go
@@ -66,7 +66,7 @@ func Test_parseInputData(t *testing.T) {
 	}
 }
 
-func TestScratchcard_calculatePoints(t *testing.T) {
+func TestScratchcard_evaluateScratchcards(t *testing.T) {
 	tests := []struct {
 		name     string
 		input    []scratchcard
@@ -114,13 +114,63 @@ func TestScratchcard_calculatePoints(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			actual := make([]int, 0)
-			for _, scratchcard := range tt.input {
-				actual = append(actual, scratchcard.calculatePoints())
-			}
+			actual := evaluateScratchcards(tt.input)
 			if !reflect.DeepEqual(tt.expected, actual) {
 				t.Errorf("expected: %+v, got: %+v", tt.expected, actual)
 			}
 		})
 	}
+}
+
+func Test_pathThroughScratchcards(t *testing.T) {
+	tests := []struct {
+		name              string
+		allScratchcards   []scratchcard
+		expectedFinalHand map[int]int
+	}{
+		{
+			name: "sample",
+			allScratchcards: []scratchcard{
+				{
+					id:            1,
+					winningValues: []int{41, 48, 83, 86, 17},
+					actualValues:  []int{83, 86, 6, 31, 17, 9, 48, 53},
+				},
+				{
+					id:            2,
+					winningValues: []int{13, 32, 20, 16, 61},
+					actualValues:  []int{61, 30, 68, 82, 17, 32, 24, 19},
+				},
+				{
+					id:            3,
+					winningValues: []int{1, 21, 53, 59, 44},
+					actualValues:  []int{69, 82, 63, 72, 16, 21, 14, 1},
+				},
+				{
+					id:            4,
+					winningValues: []int{41, 92, 73, 84, 69},
+					actualValues:  []int{59, 84, 76, 51, 58, 5, 54, 83},
+				},
+				{
+					id:            5,
+					winningValues: []int{87, 83, 26, 28, 32},
+					actualValues:  []int{88, 30, 70, 12, 93, 22, 82, 36},
+				},
+				{
+					id:            6,
+					winningValues: []int{31, 18, 13, 56, 72},
+					actualValues:  []int{74, 77, 10, 23, 35, 67, 36, 11},
+				},
+			},
+			expectedFinalHand: map[int]int{
+				1: 1,
+				2: 2,
+				3: 4,
+				4: 8,
+				5: 14,
+				6: 1,
+			},
+		},
+	}
+
 }
