c1f3a8e0a6a27d015d5968a9045717bb3a032dea
tomaskul <>
2023-12-09 13:04:08 +0000

    Add 7.2 solution

--- solutions/day07/day07.go
+++ solutions/day07/day07.go
@@ -13,121 +13,18 @@ type Day07 struct {
 	rows []string
 }
 
-var cardFaces = []rune{'A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2'}
+var cardFacesPt1 = []rune{'A', 'K', 'Q', 'J', 'T', '9', '8', '7', '6', '5', '4', '3', '2'}
+var cardFacesPt2 = []rune{'A', 'K', 'Q', 'T', '9', '8', '7', '6', '5', '4', '3', '2', 'J'}
 
-// const (
-// 	HighCardType = iota
-// 	OnePairType
-// 	TwoPairType
-// 	ThreeOfKindType
-// 	FourOfKindType
-// 	FiveOfKindType
-// )
-
-type item struct {
-	hand card
-	bid  int
-}
-
-type card struct {
-	value  string
-	lookup map[byte]int
-}
-
-func newCard(input string) card {
-	lookup := map[byte]int{}
-	for i := 0; i < len(input); i++ {
-		_, ok := lookup[input[i]]
-		if ok {
-			lookup[input[i]] += 1
-		} else {
-			lookup[input[i]] = 1
-		}
-	}
-	return card{
-		value:  input,
-		lookup: lookup,
-	}
-}
-
-func (c card) getType() int {
-	if c.isFiveOfAKind() {
-		return 6
-	} else if c.isFourOfAKind() {
-		return 5
-	} else if c.isFullHouse() {
-		return 4
-	} else if c.isThreeOfAKind() {
-		return 3
-	} else if c.isTwoPair() {
-		return 2
-	} else if c.isOnePair() {
-		return 1
-	} else if c.isHighCard() {
-		return 0
-	} else {
-		fmt.Printf("debug: unable to determine %q type\n", c.value)
-		return -1
-	}
-}
-
-func (c card) isFiveOfAKind() bool {
-	return len(c.lookup) == 1
-}
-
-func (c card) isFourOfAKind() bool {
-	return len(c.lookup) == 2 &&
-		(c.lookup[c.value[0]] == 1 || c.lookup[c.value[0]] == 4)
-}
-
-func (c card) isFullHouse() bool {
-	return len(c.lookup) == 2 &&
-		(c.lookup[c.value[0]] == 3 || c.lookup[c.value[0]] == 2)
-}
-
-func (c card) isThreeOfAKind() bool {
-	if len(c.lookup) != 3 {
-		return false
-	}
-	for _, v := range c.lookup {
-		if v == 2 {
-			return false
-		}
-	}
-	return true
-}
-
-func (c card) isTwoPair() bool {
-	if c.isFiveOfAKind() || c.isFourOfAKind() || c.isThreeOfAKind() {
-		return false
-	}
-
-	pairOne := false
-	for _, v := range c.lookup {
-		if v == 2 {
-			if !pairOne {
-				pairOne = true
-			} else {
-				return true
-			}
-		}
-	}
-	return false
-}
-
-func (c card) isOnePair() bool {
-	pairCounts := 0
-	for _, v := range c.lookup {
-		if v == 2 {
-			pairCounts++
-		}
-	}
-	return pairCounts == 1
-}
-
-func (c card) isHighCard() bool {
-	return len(c.lookup) == 5
-}
+const (
+	HighCardType = iota
+	OnePairType
+	TwoPairType
+	ThreeOfKindType
+	FullHouseType
+	FourOfKindType
+	FiveOfKindType
+)
 
 func NewDay07Solution(sessionCookie string) *Day07 {
 	rows, _ := util.GetCachedRows("https://adventofcode.com/2023/day/7/input", "7", ".txt", sessionCookie)
@@ -137,10 +34,10 @@ func NewDay07Solution(sessionCookie string) *Day07 {
 }
 
 func (s *Day07) PrintPart1() {
-	fmt.Println(calculateWinnings(sortPt1(parsePt1(s.rows))))
+	fmt.Println(calculateWinnings(sortPt1(parse(s.rows))))
 }
 
-func parsePt1(rows []string) []item {
+func parse(rows []string) []item {
 	items := make([]item, len(rows))
 	for i, row := range rows {
 		tokens := strings.Split(row, " ")
@@ -156,18 +53,18 @@ func parsePt1(rows []string) []item {
 
 func sortPt1(items []item) []item {
 	sort.Slice(items, func(i, j int) bool {
-		iType, jType := items[i].hand.getType(), items[j].hand.getType()
+		iType, jType := items[i].hand.getType(false), items[j].hand.getType(false)
 		if iType == jType {
 
 			iValue, jValue := -1, -1
 			for k := 0; k < 5; k++ {
-				for idx, face := range cardFaces {
+				for idx, face := range cardFacesPt1 {
 					//cardFaces are in descending value.
 					if items[i].hand.value[k] == byte(face) {
-						iValue = len(cardFaces) - idx
+						iValue = len(cardFacesPt1) - idx
 					}
 					if items[j].hand.value[k] == byte(face) {
-						jValue = len(cardFaces) - idx
+						jValue = len(cardFacesPt1) - idx
 					}
 				}
 				if iValue == jValue {
@@ -178,6 +75,37 @@ func sortPt1(items []item) []item {
 
 			}
 			return iValue < jValue
+		} else {
+			return iType < jType
+		}
+	})
+	return items
+}
+
+func sortPt2(items []item) []item {
+	sort.Slice(items, func(i, j int) bool {
+		iType, jType := items[i].hand.getType(true), items[j].hand.getType(true)
+		if iType == jType {
+			iValue, jValue := -1, -1
+			for k := 0; k < 5; k++ {
+				for idx, face := range cardFacesPt2 {
+					//cardFaces are in descending value.
+					if items[i].hand.value[k] == byte(face) {
+						iValue = len(cardFacesPt2) - idx
+					}
+					if items[j].hand.value[k] == byte(face) {
+						jValue = len(cardFacesPt2) - idx
+					}
+				}
+				if iValue == jValue {
+					continue
+				} else {
+					break
+				}
+			}
+
+			return iValue < jValue
+
 		} else {
 			return iType < jType
 		}
@@ -196,4 +124,5 @@ func calculateWinnings(items []item) int {
 }
 
 func (s *Day07) PrintPart2() {
+	fmt.Println(calculateWinnings(sortPt2(parse(s.rows))))
 }
--- solutions/day07/day07_test.go
+++ solutions/day07/day07_test.go
@@ -5,30 +5,6 @@ import (
 	"testing"
 )
 
-// func TestCard_getType(t *testing.T) {
-// 	tests := []struct {
-// 		name         string
-// 		input        string
-// 		expectedType int
-// 	}{
-// 		{
-// 			name:         "sample - one pair",
-// 			input:        "32T3K",
-// 			expectedType: -1,
-// 		},
-// 	}
-
-// 	for _, tc := range tests {
-// 		t.Run(tc.name, func(t *testing.T) {
-// 			sut := newCard(tc.input)
-// 			actual := sut.getType()
-// 			if tc.expectedType != actual {
-// 				t.Errorf("expected: %d, got: %d", tc.expectedType, actual)
-// 			}
-// 		})
-// 	}
-// }
-
 func Test_sortPt1(t *testing.T) {
 	tests := []struct {
 		name         string
@@ -48,8 +24,42 @@ func Test_sortPt1(t *testing.T) {
 
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
-			items := parsePt1(tc.input)
+			items := parse(tc.input)
 			sorted := sortPt1(items)
+
+			actual := make([]int, 5)
+			for i := 0; i < len(sorted); i++ {
+				actual[i] = sorted[i].bid
+			}
+
+			if !reflect.DeepEqual(tc.expectedBids, actual) {
+				t.Errorf("expected: %v, got: %v", tc.expectedBids, actual)
+			}
+		})
+	}
+}
+
+func Test_sortPt2(t *testing.T) {
+	tests := []struct {
+		name         string
+		input        []string
+		expectedBids []int
+	}{
+		{
+			name: "sample",
+			input: []string{"32T3K 765",
+				"T55J5 684",
+				"KK677 28",
+				"KTJJT 220",
+				"QQQJA 483"},
+			expectedBids: []int{765, 28, 684, 483, 220},
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			items := parse(tc.input)
+			sorted := sortPt2(items)
 
 			actual := make([]int, 5)
 			for i := 0; i < len(sorted); i++ {
@@ -82,7 +92,7 @@ func Test_calculateWinnings(t *testing.T) {
 
 	for _, tc := range tests {
 		t.Run(tc.name, func(t *testing.T) {
-			items := parsePt1(tc.input)
+			items := parse(tc.input)
 			sortedItems := sortPt1(items)
 			actual := calculateWinnings(sortedItems)
 
